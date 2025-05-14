b021e8e4c7463d6d8fca5afa62785c3828543db2
Jimmy Tithphit <518275@student.saxion.nl>
2025-05-14 13:02:43 +0200

    patientProfile created

---
+++ Models/Patient.cs
@@ -0,0 +1,12 @@
+﻿namespace Itrack_Iwatch.Models  
+{
+    public class Patient
+    {
+        public string ProfilePictureUrl { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string Sickness { get; set; }
+        public string Contact {  get; set; }
+    }
+}
