0e54aa96d1d01c8ead25dc2c5e95947af3843c2b
Rizame <igskade@gmail.com>
2025-06-07 16:32:51 +0200

    api integration for bpm done

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace SmartWatchWeb
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 			builder.Services.AddSingleton<userState>();
 
+
 			await builder.Build().RunAsync();
         }
     }
