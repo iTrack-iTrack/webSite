f8c011be29f2da821a0ea7285b733de548e940d9
Jimmy Tithphit <518275@student.saxion.nl>
2025-06-07 13:20:57 +0200

    register done, login styling, account page done

--- Models/Patient.cs
+++ Models/Patient.cs
@@ -4,6 +4,15 @@
     {
         public string ProfilePictureUrl { get; set; }
         public string Name { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+
+
         public DateTime DateOfBirth { get; set; }
         public string Address { get; set; }
         public string Sickness { get; set; }
