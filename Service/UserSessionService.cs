9cf14042c2a6f6b5bd218253fff42236510fe7e8
Jimmy Tithphit <518275@student.saxion.nl>
2025-06-07 16:50:38 +0200

    usersession

---
+++ Service/UserSessionService.cs
@@ -0,0 +1,19 @@
+﻿public class UserSessionService
+{
+    public string LoggedInUsername { get; private set; }
+
+    public void SetLoggedInUsername(string username)
+    {
+        LoggedInUsername = username;
+    }
+    public void ClearUsername()
+    {
+        LoggedInUsername = null;
+    }
+
+
+    public void Clear()
+    {
+        LoggedInUsername = null;
+    }
+}
