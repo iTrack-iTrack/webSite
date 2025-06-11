28bc60aae9d645db0a79c3530b63553d1584c593
Rizame <igskade@gmail.com>
2025-06-11 14:30:01 +0200

    live data working

--- Services/HealthDataService.cs
+++ Services/HealthDataService.cs
@@ -8,6 +8,8 @@ namespace SmartWatchWeb.Services
 		int Oxidation { get; set; }
 		int Steps { get; set; }
         float Temp { get; set; }
+
+		bool fall {  get; set; }
         Queue<TimePoint> BpmHistory { get; }
 		void AddBpmDataPoint(int bpm);
 
@@ -17,9 +19,11 @@ namespace SmartWatchWeb.Services
 		private const int MaxDataPoints = 30;
 		public Queue<TimePoint> BpmHistory { get; } = new();
 		public int CurrentBpm { get; private set; }
-		public int Oxidation { get; set; } = 77; // static placeholder
-		public int Steps { get; set; } = 1000;//static placeholder
+		public int Oxidation { get; set; } = 77;
+		public int Steps { get; set; } = 1000;
         public float Temp { get; set; } = 0;
+
+        public bool fall { get; set; }
 
         public void AddBpmDataPoint(int bpm)
 		{
